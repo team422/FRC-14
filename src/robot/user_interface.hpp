7289875ad2ff940496fb7e70e55cd0716c2b5d0d
Lucario <smpaaiml2000-a@yahoo.com>
2014-01-11 18:35:35 -0500

    First Working Copy

--- src/robot/user_interface.hpp
+++ src/robot/user_interface.hpp
@@ -1,9 +1,12 @@
+#ifndef USER_INTERFACE_HPP
+#define USER_INTERFACE_HPP
 #include <WPILib.h>
 #include "port_mapping.hpp"
 
 namespace UI {
 	namespace Primary_Driver {
-		Joystick * const left_joystick  = new Joystick(1);
-		Joystick * const right_joystick = new Joystick(2);
+		extern Joystick * left_joystick;
+		extern Joystick * right_joystick;
 	}
 }
+#endif
