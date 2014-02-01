dc59dedbc89e14135be77831c4d9a8938f138243
William Kunkel <will@wkunkel.com>
2014-02-01 17:05:56 -0500

    Added compressor and vision networking

--- src/robot/subsystems/compressor_system.cpp
+++ src/robot/subsystems/compressor_system.cpp
@@ -5,6 +5,6 @@
 Compressor_System::Compressor_System() :
 	Subsystem("Compressor_System"),
 	compressor( new Compressor(Ports::Digital_IO::PRESSURE_SWITCH,
-				   Ports::Relays::COMPRESSOR) ) {
+	                           Ports::Relays::COMPRESSOR) ) {
 	compressor->Start();
 }
