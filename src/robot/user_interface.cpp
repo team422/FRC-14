2ba30a61019336c22b954df46c8d3da6008884ed
William Kunkel <will@wkunkel.com>
2014-02-03 16:28:37 -0500

    Moved to xbox controller for primary

--- src/robot/user_interface.cpp
+++ src/robot/user_interface.cpp
@@ -1,6 +1,11 @@
 #include "user_interface.hpp"
 #include <WPILib.h>
 
-Joystick * UI::Primary_Driver::left_joystick = new Joystick(1);
-Joystick * UI::Primary_Driver::right_joystick = new Joystick(2);
-Joystick * UI::Secondary_Driver::joystick = new Joystick(3);
+Joystick * UI::Primary_Driver::left_analog_stick = new Joystick(1);
+Joystick * UI::Primary_Driver::right_analog_stick = new Joystick(1);
+Joystick * UI::Secondary_Driver::joystick = new Joystick(2);
+
+void UI::initialize() {
+	Primary_Driver::right_analog_stick->SetAxisChannel(Joystick::kXAxis, 4);
+	Primary_Driver::right_analog_stick->SetAxisChannel(Joystick::kYAxis, 5);
+}
