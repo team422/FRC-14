361eab45cd570dd104f31ac67b74318f1ef0c9a7
interiorcrocodilealligator <davidmclayton0@gmail.com>
2014-02-04 17:05:26 -0500

    started button thing

--- src/robot/user_interface.cpp
+++ src/robot/user_interface.cpp
@@ -1,11 +1,15 @@
 #include "user_interface.hpp"
 #include <WPILib.h>
 
+#include "Commands/toggle_shifter.hpp"
+
 Joystick * UI::Primary_Driver::left_analog_stick = new Joystick(1);
 Joystick * UI::Primary_Driver::right_analog_stick = new Joystick(1);
+JoystickButton * UI::Primary_Driver::shift_button = new JoystickButton(left_analog_stick, 5);
 Joystick * UI::Secondary_Driver::joystick = new Joystick(2);
 
 void UI::initialize() {
 	Primary_Driver::right_analog_stick->SetAxisChannel(Joystick::kXAxis, 4);
 	Primary_Driver::right_analog_stick->SetAxisChannel(Joystick::kYAxis, 5);
+	Primary_Driver::shift_button->WhenPressed( new ToggleShifter() );
 }
