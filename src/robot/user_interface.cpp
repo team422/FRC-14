f333e3acc813ba2e1f6e2d70026998c3d5965714
Ryan Lucia <ryan@luciaonline.net>
2014-04-11 18:32:41 -0400

    Updated to reflect options for controller
    Now supports both xbox and built-in Joystick class, to use xbox for primary driver define USE_XBOX_PRIMARY

--- src/robot/user_interface.cpp
+++ src/robot/user_interface.cpp
@@ -10,18 +10,41 @@
 #include "subsystems/subsystems.hpp"
 #include <WPILib.h>
 
+// When developing, if a binding has not been determined, it can be replaced with
+// `UNKNOWN`. This will allow the code to compile in debug mode, but in release
+// mode it will throw an error.
+#ifdef DEBUG
+#define UNKNOWN 0
+#else 
+#define UNKNOWN @bind_not_assigned
+#endif
+
+#ifdef USE_XBOX_PRIMARY
 Xbox_Controller * UI::Primary_Driver::controller = new Xbox_Controller(1);
+#else 
+Joystick * UI::Primary_Driver::controller = new Joystick(1);
+#endif
 Xbox_Controller * UI::Secondary_Driver::controller = new Xbox_Controller(2);
 
 // Set up all the button bindings for the drivers
 void UI::initialize() {
+#ifdef USE_XBOX_PRIMARY
+    Legacy code for Xbox controller
+    Primary_Driver::controller->
+	    LEFT_BUMPER->WhenPressed( new Toggle_Shifter() );
+    Primary_Driver::controller->
+    	Y->WhenPressed( new Reverse_Drive() );
+    Primary_Driver::controller->
+    	RIGHT_BUMPER->WhenPressed( new Toggle_Collector() );
+#else 
 	Primary_Driver::controller->
-		LEFT_BUMPER->WhenPressed( new Toggle_Shifter() );
+	    UNKNOWN->WhenPressed( new Toggle_Shifter() );
 	Primary_Driver::controller->
-		Y->WhenPressed( new Reverse_Drive() );
+	    UNKNOWN->WhenPressed( new Reverse_Drive() );
 	Primary_Driver::controller->
-		RIGHT_BUMPER->WhenPressed( new Toggle_Collector() );
-	
+	    UNKNOWN->WhenPressed( new Toggle_Collector() )
+#endif
+
 	Secondary_Driver::controller->
 		A->WhenPressed( new Toggle_Catapult_Lock() );
 	Secondary_Driver::controller->
