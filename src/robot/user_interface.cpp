bc538289a33b5d852107ad582ef703d7e7863994
William Kunkel <will@wkunkel.com>
2014-04-23 01:43:43 -0400

    cleaned up user_interface.cpp with using declarations

--- src/robot/user_interface.cpp
+++ src/robot/user_interface.cpp
@@ -11,27 +11,27 @@ Joystick *UI::Primary_Driver::right_stick = new Joystick(2);
 
 KinectStick *UI::Primary_Driver::left_arm = new KinectStick(1);
 KinectStick *UI::Primary_Driver::right_arm = new KinectStick(2);
- 
+
 Xbox_Controller *UI::Secondary_Driver::controller = new Xbox_Controller(2);
 
 // Set up all the button bindings for the drivers
 void UI::initialize() {
-	Primary_Driver::toggle_shifter_button =
-		new JoystickButton(Primary_Driver::left_stick, 0);
-	Primary_Driver::reverse_drive_button =
-		new JoystickButton(Primary_Driver::left_stick, 0);
-	Primary_Driver::lower_collector_button =
-		new JoystickButton(Primary_Driver::left_stick, 0);
+	{
+		using namespace Primary_Driver;
+		
+    	toggle_shifter_button = new JoystickButton(left_stick, 0);
+		reverse_drive_button = new JoystickButton(left_stick, 0);
+		lower_collector_button = new JoystickButton(left_stick, 0);
 
-	Primary_Driver::toggle_shifter_button->
-		WhenPressed( new Toggle_Shifter() );
-	Primary_Driver::reverse_drive_button->
-		WhenPressed( new Reverse_Drive() );
-	Primary_Driver::lower_collector_button->
-		WhileHeld( new Lower_Collector() );
-	
-	Secondary_Driver::controller->
-		RIGHT_BUMPER->WhileHeld( new Retract_Piston() );
-	Secondary_Driver::controller->
-		LEFT_BUMPER->WhileHeld( new Lower_Catapult() );
+		toggle_shifter_button->WhenPressed( new Toggle_Shifter() );
+		reverse_drive_button->WhenPressed( new Reverse_Drive() );
+		lower_collector_button->WhileHeld( new Lower_Collector() );
+	}
+
+	{
+		using namespace Secondary_Driver;
+		
+		controller->RIGHT_BUMPER->WhileHeld( new Retract_Piston() );
+		controller->LEFT_BUMPER->WhileHeld( new Lower_Catapult() );
+	}
 }
